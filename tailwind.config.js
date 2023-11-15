7615c19c287c645f1ba3a73ce9a474a63683f187
iziz9 <46iz.us2@gmail.com>
2023-11-15 21:50:30 +0900

    Env: Setting tailwind, eslint, prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,21 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        xs: '400px',
+        sm: '540px',
+        md: '833px',
+        lg: '1024px'
+      },
+      fontSize: {
+        xxs: '0.6rem',
+        xs: '0.75rem',
+        sm: '0.75rem',
+        base: '1rem',
+        lg: '1.25rem'
+      }
+    },
   },
   plugins: [],
 }
