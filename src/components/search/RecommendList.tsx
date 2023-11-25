65866c8e683b533525e6f8b7b101c8d47a1bd8e3
iziz9 <46iz.us2@gmail.com>
2023-11-25 16:04:31 +0900

    Feat: react-slick setting

--- src/components/search/RecommendList.tsx
+++ src/components/search/RecommendList.tsx
@@ -1,7 +1,24 @@
 import React from 'react'
+import styled from 'styled-components'
+import Slider from 'react-slick'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
 
 const RecommendList = () => {
-  return <div>RecommendList</div>
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  }
+  return (
+    <RecommendSection>
+      <div>이런 영화는 어떠세요?</div>
+    </RecommendSection>
+  )
 }
+
+const RecommendSection = styled.section``
 
 export default RecommendList
